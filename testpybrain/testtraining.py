bc66670b031b0a6889ac7ba509a3c4c8128d9e14
andresosw jiemenz <andres_osw@hotmail.com>
2016-03-26 14:49:18 -0430

    added command line params support, utilities module

--- testpybrain/testtraining.py
+++ testpybrain/testtraining.py
@@ -2,10 +2,14 @@ from pybrain.datasets            import ClassificationDataSet
 from pybrain.tools.shortcuts     import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 
+import os
 from os import listdir
 from os.path import isfile,join
 import numpy as np
 import random
+
+from utilities import extractCommandParams,createRunFolder,writeAsJson
+import sys
 
 SIGNAL_LENGTH = 15
 SIGNAL_COUNT = 1
@@ -18,7 +22,6 @@ def getTrainingData(dirname):
 		target = 1
 	elif dirname.startswith('female'):
 		target = 0
-	print target
 	for index,file_name in enumerate(listdir(dirname)):
 		data_file = join(dirname,file_name)
 		if (not isfile(data_file)) or ('.mfcc' not in file_name): continue
@@ -79,14 +82,52 @@ def test_neuralnetwork(dirname,neuralNetwork):
 		result = neuralNetwork.activate(vs[0])
 		print file_name, 'classified as', getGender(result[0]), 'with', result[0], ' output'
 
-if __name__ == '__main__':
+"""
+	Main training function
+"""
+def trainGenderClassification(learningRate,hiddenNeurons,bias,maxIterations,femaleDataDir,
+							maleDataDir,signalLength,signalCount,resultsFolder):
+
+	print '----------------------------------------------------------------'
+	print '***** Running Backpropagation Trainer with parameters:\n'
+	print '* learningRate   : %s' %(learningRate)
+	print '* hiddenNeurons  : %s' %(hiddenNeurons)
+	print '* bias           : %s' %(bias)
+	print '* maxIterations  : %s' %(maxIterations)
+	print '* femaleDataDir  : %s' %(femaleDataDir)
+	print '* maleDataDir    : %s' %(maleDataDir) 
+	print '* signalLength   : %s' %(signalLength)
+	print '* signalCount    : %s' %(signalCount)
+	print '* resultsFolder  : %s' %(resultsFolder) 
+	print '----------------------------------------------------------------'
+	"""
+		Computing results folder
+	"""
+	run_path = createRunFolder(resultsFolder=resultsFolder)
+
+	print '**** Dumping results in directory: \"%s\"' %(run_path)
+	print '----------------------------------------------------------------'
+	input_params_file = os.path.join(run_path,'inputParams.txt')
+	input_params = {
+		'learningRate': learningRate,
+		'hiddenNeurons': hiddenNeurons,
+		'bias': bias,
+		'maxIterations': maxIterations,
+		'femaleDataDir': femaleDataDir,
+		'maleDataDir': maleDataDir,
+		'signalLength': signalLength,
+		'signalCount':signalCount,
+		'resultsFolder':resultsFolder
+	}
+	writeAsJson(input_params,input_params_file)
+
 	#obtaining female_data
-	female_data = getTrainingData('female_big')
+	female_data = getTrainingData(femaleDataDir)
 	female_input = female_data[0]
 	female_target =  female_data[1]
 
 	#obtanining male data
-	male_data = getTrainingData('male_big')
+	male_data = getTrainingData(maleDataDir)
 	male_input = male_data[0]
 	male_target =  male_data[1]
 
@@ -105,29 +146,23 @@ if __name__ == '__main__':
 	for samplenum in xrange(0,len(training_input)):
 		training_dataset.addSample(training_input[samplenum],[training_target[samplenum]])
 
-
-	#building up the network 
-	NUMBER_OF_HIDDEN_NEURONS = 200
-	LEARNING_RATE = 0.01
-	MAX_ITERATIONS = 750
-
 	print "Number of training patterns: %s" %(len(training_dataset))
 	print "Input dimension: %s" %(training_dataset.indim)
 	print "Output dimension: %s" %(training_dataset.outdim)
-	print "Number of hidden Neurons %s" %(NUMBER_OF_HIDDEN_NEURONS)
-	print "Learning Rate %s" %(LEARNING_RATE)
+	print "Number of hidden Neurons %s" %(hiddenNeurons)
+	print "Learning Rate %s" %(learningRate)
 
-	network = buildNetwork( training_dataset.indim,NUMBER_OF_HIDDEN_NEURONS, training_dataset.outdim, bias=True )
-	trainer = BackpropTrainer(network,training_dataset,learningrate = LEARNING_RATE, verbose = False)
+	network = buildNetwork( training_dataset.indim,hiddenNeurons, training_dataset.outdim, bias=bias )
+	trainer = BackpropTrainer(network,training_dataset,learningrate = learningRate, verbose = False)
 
 	#training with training dataset
-	for epoch in xrange(0,MAX_ITERATIONS):
+	for epoch in xrange(0,maxIterations):
 		epoch_error = trainer.train()
 		print '%s Train Error: %s Train Accuracy: %s' %(epoch,epoch_error,1-epoch_error)
 
 	# Testing network on directories
-	male_dir = 'male_big'
-	female_dir = 'female_big'
+	male_dir = maleDataDir
+	female_dir = femaleDataDir
 	test_dir = 'test'
 	
 	print 'Test for %s directory' % (male_dir)
@@ -138,3 +173,43 @@ if __name__ == '__main__':
 
 	print 'Test for %s directory' % (test_dir)
 	test_neuralnetwork(test_dir,network)
+	 
+
+if __name__ == '__main__':
+	arguments = extractCommandParams(sys.argv[1:]) 
+
+	#mandatory args
+	learningRate = arguments["learningrate"]
+	hiddenNeurons = arguments["hiddenneurons"]
+	bias = arguments["bias"]
+	maxIterations = arguments["iterations"]
+	femaleDataDir = arguments["femaledir"]
+	maleDataDir = arguments["maledir"]
+	global SIGNAL_LENGTH,SIGNAL_COUNT
+
+	#optional args
+	DEFAULT_SIGNAL_LENGTH = 15
+	DEFAULT_SIGNAL_COUNT = 1
+	DEFAULT_RESULTS_FOLDER = 'gender-class-runs' #default name of folder where to place the result files
+
+	if "signallength" in arguments:
+	  signalLength = arguments["signallength"]
+	else:
+	  signalLength = DEFAULT_SIGNAL_LENGTH
+
+	if "signalcount" in arguments:
+	  signalCount = arguments["signalcount"]
+	else:
+	  signalCount = DEFAULT_SIGNAL_COUNT
+
+	if "rfolder" in arguments:
+	  resultsFolder = arguments["rfolder"]
+	else:
+	  resultsFolder = DEFAULT_RESULTS_FOLDER
+
+	SIGNAL_LENGTH = signalLength
+	SIGNAL_COUNT = signalCount
+	trainGenderClassification(learningRate=learningRate,hiddenNeurons=hiddenNeurons,bias=bias,
+							maxIterations=maxIterations,femaleDataDir=femaleDataDir,
+							maleDataDir=maleDataDir,signalLength=signalLength,
+							signalCount=signalCount,resultsFolder=resultsFolder)
---
+++ testpybrain/utilities.py
@@ -0,0 +1,134 @@
+import sys, getopt
+import os
+import json
+"""
+   Utilities module
+"""
+
+def writeAsJson(content,file,indent=0):
+   with open(file, 'w') as outfile:
+      json.dump(content, outfile,indent=indent)
+
+"""
+   @return a dictionary of parsed arguments
+"""
+def extractCommandParams(argv):
+   how_to_use_message = '$ Usage: \n\tShort ARGS: testtraining.py -l <rate> ' \
+                        '-h <howmany> -b <true,false> -i <howmany> -f <path> -m <path>\n'\
+                        '\tLong  ARGS: testtraining.py --learningrate <rate> ' \
+                        '--hiddenneurons <howmany> --bias <true,false> --iterations <howmany>'\
+                        '--femaledir <path> --maledir <path>\n\n'\
+                        '\t[OPTIONAL ARGS] --signallength <length> --signalcount <count> --checkclassdir <parh> --rfolder <path> '\
+                        '--elitism <true,false> --pselection <roulette,rank,tournament>\n\n'\
+                        '$ Please refer to the README.md file for further explanation\n'
+
+   mandatory_args = [("-l","--learningrate"),("-h","--hiddenneurons"),("-b","--bias"),("-i","--iterations"),("-f","--femaledir"),("-m","--maledir")]
+   optional_args = [("--signallength"),("--signalcount"),("--checkclassdir"),("--rfolder")]
+
+   # checking that all mandatory arguments were provide within the command line
+   for shortArg,longArg in mandatory_args:
+      if ((not shortArg in argv) and (not longArg in argv)):
+         print "\n$ Execution Error: Missing argument \"%s\"" %(longArg)
+         print how_to_use_message
+         sys.exit(2)
+  
+   try:
+      opts, args = getopt.getopt(argv,'l:h:b:i:f:m:',['learningrate=','hiddenneurons=','bias=','iterations=',
+      												'femaledir=','maledir=','signallength=','signalcount=','checkclassdir=',
+      												'rfolder='])
+   except getopt.GetoptError:
+      print how_to_use_message
+      sys.exit(2)
+
+   parsed_arguments = {}
+
+   for opt, arg in opts:
+      #mandatory args
+      if opt in ("-l","--learningrate"):
+         parsed_arguments["learningrate"] = float(arg)
+      elif opt in ("-h","--hiddenneurons"):
+         parsed_arguments["hiddenneurons"] = int(arg)
+      elif opt in ("-b","--bias"):
+         parsed_arguments["bias"] = bool(arg)
+      elif opt in ("-i","--iterations"):
+         parsed_arguments["iterations"] = int(arg)
+      elif opt in ("-f","--femaledir"):
+         parsed_arguments["femaledir"] = arg
+      elif opt in ("-m","--maledir"):
+         parsed_arguments["maledir"] = arg
+
+      #optional args
+      elif opt == "--signallength":
+         parsed_arguments["signallength"] = int(arg)
+      elif opt == "--signalcount":
+         parsed_arguments["signalcount"] = int(arg)
+      elif opt == "--checkclassdir":
+      	parsed_arguments["checkclassdir"] = arg
+      elif opt == "--rfolder":
+      	parsed_arguments["rfolder"] = arg
+
+   return parsed_arguments
+
+
+"""
+   Creates a folder where to dump run results. Runs have the same prefix and are identified
+   by an index starting at 0.
+   @param resultsFolder the parent folder
+   @param runFolderPrefix a prefix to be used to name run folders
+   @return the path pointing to the created directory
+"""
+def createRunFolder(resultsFolder,runFolderPrefix='run_'):
+      """
+         Computing run folder
+      """
+      found = False
+      dir_index = 0
+      dir_prefix = os.path.join(resultsFolder,runFolderPrefix)
+
+      #find an unused index-based name for the new run folder
+      while not found:
+         run_path = dir_prefix+str(dir_index)
+         found =  not os.path.isdir(run_path)
+         dir_index += 1
+
+      #create the directory
+      try: 
+          os.makedirs(run_path)
+      except OSError:
+          if not os.path.isdir(run_path):
+              raise
+      return run_path
+
+if __name__ == "__main__":
+   #ignoring the name of the program from the command line args
+   arguments = extractCommandParams(sys.argv[1:]) 
+
+   #mandatory args
+   learningRate = arguments["learningrate"]
+   hiddenNeurons = arguments["hiddenneurons"]
+   bias = arguments["bias"]
+   maxIterations = arguments["iterations"]
+   female_data_dir = arguments["femaledir"]
+   male_data_dir = arguments["maledir"]
+
+   #optional args
+   DEFAULT_SIGNAL_LENGTH = 15
+   DEFAULT_SIGNAL_COUNT = 1
+   DEFAULT_RESULTS_FOLDER = 'gender-class-runs' #default name of folder where to place the result files
+
+   if "signallength" in arguments:
+      signal_length = arguments["signallength"]
+   else:
+      signal_length = DEFAULT_SIGNAL_LENGTH
+
+   if "signalcount" in arguments:
+      signal_count = arguments["signalcount"]
+   else:
+      signal_count = DEFAULT_SIGNAL_COUNT
+
+   if "rfolder" in arguments:
+      results_folder = arguments["rfolder"]
+   else:
+      results_folder = DEFAULT_RESULTS_FOLDER 
+
+   print arguments
